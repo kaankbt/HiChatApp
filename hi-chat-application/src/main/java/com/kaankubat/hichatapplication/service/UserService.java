b207f8446655bc0d41152dbb693068574df5262f
Kaan <170103074@ogr.alanya.edu.tr>
2022-01-25 00:28:39 +0300

    Update | service

---
+++ hi-chat-application/src/main/java/com/kaankubat/hichatapplication/service/UserService.java
@@ -0,0 +1,110 @@
+package com.kaankubat.hichatapplication.service;
+
+import com.kaankubat.hichatapplication.model.BlockModel;
+import com.kaankubat.hichatapplication.model.User;
+import com.kaankubat.hichatapplication.repository.ActivityLogModelRepo;
+import com.kaankubat.hichatapplication.repository.BlockRepository;
+import com.kaankubat.hichatapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserService implements UserServiceInterface{
+
+    private final UserRepository userRepo;
+    private final ActivityLogModelRepo activityLogEntityRepo;
+    private final BlockRepository blockedUserRepo;
+
+    @Autowired
+    public UserService(UserRepository userRepo, ActivityLogModelRepo activityLogEntityRepo, BlockRepository blockedRepo) {
+        this.userRepo = userRepo;
+        this.activityLogEntityRepo = activityLogEntityRepo;
+        this.blockedUserRepo = blockedRepo;
+    }
+
+
+    @Override
+    public Boolean block(String angryName, String blockedName) {
+        User angry = userRepo.findByUserName(angryName);
+        User blocked = userRepo.findByUserName(blockedName);
+        if(angry.getId() != null && blocked.getId() != null) {
+            BlockModel blockModel = new BlockModel();
+            blockModel.setAngryId(angry.getId());
+            blockModel.setBlockedId(blocked.getId());
+            blockedUserRepo.save(blockModel);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<User> listUsers = userRepo.findAll();
+        return listUsers;
+    }
+
+    @Override
+    public User findByEmail(String mail) {
+        User user = userRepo.findByEmail(mail);
+        return user;
+    }
+
+    @Override
+    public User findByUserName(String userName) {
+        User user = userRepo.findByUserName(userName);
+        return user;
+    }
+
+    @Override
+    public Set<String> findAllByName() {
+        List<User> listUsers = userRepo.findAll();
+        Set<String> response = new HashSet<String>();
+        listUsers.forEach(user ->{
+            String name = user.getUserName();
+            response.add(name);
+        });
+
+        return response;
+    }
+
+    @Override
+    public Boolean blockControl(String angryName, String blockedName) {
+        User angry = userRepo.findByUserName(angryName);
+        User blocked = userRepo.findByUserName(blockedName);
+        List<BlockModel> listOfBlock = blockedUserRepo.findAllByAngryId(angry.getId());
+        ArrayList<Long> blockedIds = new ArrayList<Long>();
+        int loop = listOfBlock.size();
+        for(int flag = 0; flag<loop;flag++) {
+            blockedIds.add(listOfBlock.get(flag).getBlockedId());
+        }
+        if(blockedIds.contains(blocked.getId())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean unblock(String angryName, String blockedName) {
+        User angry = userRepo.findByUserName(angryName);
+        User blocked = userRepo.findByUserName(blockedName);
+
+        if(angry.getId() != null && blocked.getId()!= null) {
+            try {
+                blockedUserRepo.unblock(angry.getId(), blocked.getId());
+            }
+            catch(Exception e){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ hi-chat-application/src/main/java/com/kaankubat/hichatapplication/service/UserServiceInterface.java
@@ -0,0 +1,24 @@
+package com.kaankubat.hichatapplication.service;
+
+import com.kaankubat.hichatapplication.model.User;
+
+import java.util.List;
+import java.util.Set;
+
+public interface UserServiceInterface {
+
+
+    Boolean block(String angry, String blocked);
+
+    Boolean unblock(String angry, String blocked);
+
+    Boolean blockControl(String angry, String blocked);
+
+    List<User> findAll();
+
+    User findByEmail(String mail);
+
+    User findByUserName(String userName);
+
+    Set<String> findAllByName();
+}
