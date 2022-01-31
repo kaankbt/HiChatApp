1e4c9a1de3a3b5b33cfe1e0d7879a93cb82c0c00
Kaan <170103074@ogr.alanya.edu.tr>
2022-01-31 20:05:18 +0300

    Update | Bug fix 1

--- hi-chat-application/src/main/java/com/kaankubat/hichatapplication/repository/ActivityLogModelRepo.java
+++
@@ -1,9 +0,0 @@
-package com.kaankubat.hichatapplication.repository;
-
-import com.kaankubat.hichatapplication.model.ActivityLogModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ActivityLogModelRepo extends JpaRepository<ActivityLogModel,Long>{
-
-}
